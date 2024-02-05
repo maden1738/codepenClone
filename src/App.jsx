a93a571a67c5037cd5e295522241440ec57c30cc
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-02-05 15:57:25 +0545

    timeout before change

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
 import Editor from "./components/Editor";
 
 function App() {
+     console.log("rendered");
      const [html, setHtml] = useState("");
      const [css, setCss] = useState("");
      const [js, setJs] = useState("");
-     const srcDoc = ` <html>
+     const [srcDoc, setSrcDoc] = useState("");
+
+     useEffect(() => {
+          const timeout = setTimeout(() => {
+               setSrcDoc(`<html>
                       <body>${html}</body>
                       <style>${css}</style>
                       <script>${js}</script>
-               </html>`;
+               </html>`);
+          }, 500);
+          return () => clearTimeout(timeout);
+     }, [html, css, js]);
      return (
           <div>
-               <div className="flex justify-between bg-black h-[60vh]">
-                    <Editor value={html} mode="html" handleChange={setHtml} />
-                    <Editor value={css} mode="css" handleChange={setCss} />
-                    <Editor value={js} mode="jsx" handleChange={setJs} />
+               <div className="flex justify-between bg-black h-[55vh]">
+                    <Editor
+                         value={html}
+                         mode="html"
+                         handleChange={setHtml}
+                         name="HTML"
+                         icon={<FaHtml5 className="text-red-500" />}
+                    />
+                    <Editor
+                         value={css}
+                         mode="css"
+                         handleChange={setCss}
+                         name="CSS"
+                         icon={<FaCss3Alt className="text-blue-500" />}
+                    />
+                    <Editor
+                         value={js}
+                         mode="jsx"
+                         handleChange={setJs}
+                         name="JS"
+                         icon={<IoLogoJavascript className="text-yellow-500" />}
+                    />
                </div>
-               <div className="h-[40vh]">
+               <div className="h-[45vh]">
                     <iframe
                          srcDoc={srcDoc}
                          frameborder="0"
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AceEditor from "react-ace";
+import { FaHtml5 } from "react-icons/fa";
 
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-css";
@@ -9,16 +10,22 @@ import "ace-builds/src-noconflict/theme-github_dark";
 import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/ext-language_tools";
 
-export default function Editor({ value, mode, handleChange }) {
+export default function Editor({ value, mode, handleChange, name, icon }) {
      return (
-          <div className="w-[32%]">
+          <div className="w-[33%]">
+               <div className="w-[100%] h-[8%]">
+                    <div className="bg-primary text-md py-2 px-4 rounded-md flex items-center gap-2 w-fit">
+                         {icon}
+                         <span className="text-white">{name}</span>
+                    </div>
+               </div>
                <AceEditor
                     width="100%"
-                    height="100%"
+                    height="92%"
                     value={value}
                     mode={mode}
                     theme="tomorrow_night"
-                    fontSize="16px"
+                    fontSize="14px"
                     // highlightActiveLine={true}
                     wrapEnabled={true}
                     setOptions={{
@@ -30,6 +37,7 @@ export default function Editor({ value, mode, handleChange }) {
                     onChange={(value) => {
                          handleChange(value);
                     }}
+                    className="border-gray-500"
                />
           </div>
      );
