b76967f8c43aa65406d3c3bfe021d904a6178ee0
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-02-05 12:57:30 +0545

    editors responsive width

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,34 @@ import { useState } from "react";
 import Editor from "./components/Editor";
 
 function App() {
-       const [html, setHtml] = useState("");
-       const [css, setCss] = useState("");
-       const [js, setJs] = useState("");
-       const srcDoc = ` <html>
+     const [html, setHtml] = useState("");
+     const [css, setCss] = useState("");
+     const [js, setJs] = useState("");
+     const srcDoc = ` <html>
                       <body>${html}</body>
                       <style>${css}</style>
                       <script>${js}</script>
                </html>`;
-       return (
-              <>
-                     <div className="flex gap-4 bg-black">
-                            <Editor
-                                   value={html}
-                                   mode="html"
-                                   handleChange={setHtml}
-                            />
-                            <Editor
-                                   value={css}
-                                   mode="css"
-                                   handleChange={setCss}
-                            />
-                            <Editor
-                                   value={js}
-                                   mode="jsx"
-                                   handleChange={setJs}
-                            />
-                     </div>
-                     <iframe
-                            srcDoc={srcDoc}
-                            frameborder="0"
-                            className="w-full"
-                     ></iframe>
-              </>
-       );
+     return (
+          <div>
+               <div className="flex justify-between bg-black h-[60vh]">
+                    <Editor value={html} mode="html" handleChange={setHtml} />
+                    <Editor value={css} mode="css" handleChange={setCss} />
+                    <Editor value={js} mode="jsx" handleChange={setJs} />
+               </div>
+               <div className="h-[40vh]">
+                    <iframe
+                         srcDoc={srcDoc}
+                         frameborder="0"
+                         height="100%"
+                         width="100%"
+                         sandbox="allow-scripts"
+                    />
+               </div>
+          </div>
+     );
 }
 
 export default App;
+
+//29 31 33
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -10,26 +10,27 @@ import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/ext-language_tools";
 
 export default function Editor({ value, mode, handleChange }) {
-       return (
-              <>
-                     <AceEditor
-                            height="500px"
-                            value={value}
-                            mode={mode}
-                            theme="tomorrow_night"
-                            fontSize="16px"
-                            // highlightActiveLine={true}
-                            wrapEnabled={true}
-                            setOptions={{
-                                   enableLiveAutocompletion: true,
-                                   showLineNumbers: true,
-                                   tabSize: 2,
-                                   autoScrollEditorIntoView: true,
-                            }}
-                            onChange={(value) => {
-                                   handleChange(value);
-                            }}
-                     />
-              </>
-       );
+     return (
+          <div className="w-[32%]">
+               <AceEditor
+                    width="100%"
+                    height="100%"
+                    value={value}
+                    mode={mode}
+                    theme="tomorrow_night"
+                    fontSize="16px"
+                    // highlightActiveLine={true}
+                    wrapEnabled={true}
+                    setOptions={{
+                         enableLiveAutocompletion: true,
+                         showLineNumbers: true,
+                         tabSize: 2,
+                         autoScrollEditorIntoView: true,
+                    }}
+                    onChange={(value) => {
+                         handleChange(value);
+                    }}
+               />
+          </div>
+     );
 }
