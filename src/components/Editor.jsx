665b0912716d0649ec3953d087b9945ebd80efc5
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-02-08 12:27:14 +0545

    clear editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,19 +3,28 @@ import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/mode-jsx";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/theme-github_dark";
 import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/ext-language_tools";
 
 export default function Editor({ value, mode, handleChange, name, icon }) {
+     function handleClick() {
+          handleChange((prev) => ({ ...prev, [name]: "" }));
+     }
+
      return (
           <div className="w-[33%]">
-               <div className="w-[100%] ">
+               <div className="w-[100%] flex justify-between">
                     <div className="bg-primary text-md py-2 px-4 rounded-t-lg flex items-center gap-2 w-fit">
                          {icon}
                          <span className="text-white uppercase">{name}</span>
                     </div>
+                    <button
+                         type="button"
+                         className="text-white mr-2"
+                         onClick={handleClick}
+                    >
+                         Clear
+                    </button>
                </div>
                <AceEditor
                     width="100%"
